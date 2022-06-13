5894ac204178052f987ffe2f3692fa4e6e0212a9
carlruan <62496966+carlruan@users.noreply.github.com>
2022-06-13 18:32:56 -0500

    2nd commit

--- src/main/java/com/example/apple/POJO/Apple.java
+++ src/main/java/com/example/apple/POJO/Apple.java
@@ -3,7 +3,17 @@ package com.example.apple.POJO;
 public class Apple {
     private String name = "I am an Apple";
 
+    private double price = 1.99;
     public String getName() {
         return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return name + " and my price is " + String.valueOf(getPrice());
     }
 }
